b975cfb47e8575b63c0365faf5e452ff8e6ae6c6
kaushalsingh7 <119290334+kaushalsingh7@users.noreply.github.com>
2024-05-01 14:33:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState, useEffect } from "react";
+import useSWR from "swr";
+import "./App.css";
+const fetcher = (...args) => fetch(...args).then((response) => response.json());
+const App = () => {
+  const [gameTitle, setGameTitle] = useState("");
+  const [searchedGames, setSearchedGames] = useState([]);
+  // const [gameDeals, setGameDeals] = useState([]);
+  const { data, error } = useSWR(
+    "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=20&pageSize=3",
+    fetcher
+  );
+  const searchGame = () => {
+    fetch(`https://www.cheapshark.com/api/1.0/games?title=${gameTitle}&limit=3`)
+      .then((response) => response.json())
+      .then((data) => {
+        setSearchedGames(data);
+      });
+  };
+  // useEffect(() => {
+  //   fetch(
+  //     `https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=20&pageSize=3`
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setGameDeals(data);
+  //     });
+  // }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="searchSection">
+        <h1>Search For A Game</h1>
+        <input
+          type="text"
+          placeholder="MineCraft......."
+          onChange={(e) => {
+            setGameTitle(e.target.value);
+          }}
+        />
+        <button onClick={searchGame}>Seach Game Title</button>
+        <div className="games">
+          {searchedGames.map((game, key) => {
+            return (
+              <div className="game" key={key}>
+                {game.external}
+                <img src={game.thumb} />
+                {game.cheapest}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="dealsSection">
+        <h1>Latest Deals</h1>
+        <div className="games">
+          {data &&
+            data.map((game, key) => {
+              return (
+                <div className="game" id="deals" key={key}>
+                  <h3>{game.title}</h3>
+                  <p>Normal Price : {game.normalPrice}</p>
+                  <p>Deal Price : {game.salePrice}</p>
+                  <h3>YOU SAVE {game.savings.substr(0, 2)}%</h3>
+                </div>
+              );
+            })}
+        </div>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
